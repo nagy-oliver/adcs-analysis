929ccb156f7232438e785f34a36cd2c29dbd1727
iplusplus-nl <admin@shitao.tech>
2025-10-13 14:39:58 +0200

    local-global

--- utils.py
+++ utils.py
@@ -1,1 +1,44 @@
 # Any util functions that might be needed
+import numpy as np
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
+
+def localToGlobal(eulerAngles, localVec):
+    phi, theta, psi = eulerAngles
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(phi), -np.sin(phi)],
+                    [0, np.sin(phi), np.cos(phi)]])
+    
+    R_y = np.array([[np.cos(theta), 0, np.sin(theta)],
+                    [0, 1, 0],
+                    [-np.sin(theta), 0, np.cos(theta)]])
+    
+    R_z = np.array([[np.cos(psi), -np.sin(psi), 0],
+                    [np.sin(psi), np.cos(psi), 0],
+                    [0, 0, 1]])
+    
+    R = R_z @ R_y @ R_x
+    globalVec = R @ localVec
+    return globalVec
+    
+def globalToLocal(eulerAngles, globalVec):
+    phi, theta, psi = eulerAngles
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(phi), -np.sin(phi)],
+                    [0, np.sin(phi), np.cos(phi)]])
+    
+    R_y = np.array([[np.cos(theta), 0, np.sin(theta)],
+                    [0, 1, 0],
+                    [-np.sin(theta), 0, np.cos(theta)]])
+    
+    R_z = np.array([[np.cos(psi), -np.sin(psi), 0],
+                    [np.sin(psi), np.cos(psi), 0],
+                    [0, 0, 1]])
+    
+    R = R_z @ R_y @ R_x
+    localVec = R.T @ globalVec
+    return localVec
