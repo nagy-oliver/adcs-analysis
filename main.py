34dd4f9133802d4682beadac28d4b7f59e40bf38
David <firedemisey@gmail.com>
2025-10-15 14:14:21 +0200

    David

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ for i in range(n):
     quaternionGlobalWRTSolar = ro.Rotation.from_euler('xyz', eulerAnglesGlobalWRTSolarDeg, degrees=True).as_quat()
     sunVectorSolar = np.array([0.4,0.7,0.5])
     sunVectorGlobal = ut.torqueGlobalToLocal(sunVectorSolar, quaternionGlobalWRTSolar)
-    torque = tq.solar_torque(sun_vector_global=sunVectorGlobal, quaternion=object.quaternion)*0+magnetic_torques*0+internal_torques+tq.torque_gg(vec_nadir=ut.torqueGlobalToLocal(cst.vec_nadir_0, object.quaternion),cst=cst)
+    torque = tq.solar_torque(sun_vector_global=sunVectorGlobal, quaternion=object.quaternion)+magnetic_torques+internal_torques+tq.torque_gg(vec_nadir=ut.torqueGlobalToLocal(cst.vec_nadir_0, object.quaternion),cst=cst)
     object.update(torque, dt=0.1)
     eulerAngleX.append(object.getEulerAnglesDeg()[0])
     eulerAngleY.append(object.getEulerAnglesDeg()[1])
